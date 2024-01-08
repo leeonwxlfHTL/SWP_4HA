3e30d8d84cf0f4fbbf28ed368c7396e861ce6b29
juliaPetz <julia.petz@hak-reutte.ac.at>
2024-01-08 19:49:40 +0100

    New classes for testing with TestNG

--- Swp_Klasse4/src/testNG/MyMath.java
+++ Swp_Klasse4/src/testNG/MyMath.java
@@ -10,4 +10,24 @@ public class MyMath {
 		return a - b;
 	}
 	
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+	
+	public int divide(int a, int b) {
+		return a / b;
+	}
+	
+	public double divide(double a, double b) {
+		return a / b;
+	}
+	
+	public boolean comparison(int a, int b) {
+//		if(a > b) {
+//			return true;
+//		}
+//		return false;
+		return a > b;
+	}
+	
 }
--- Swp_Klasse4/src/testNG/MyMathTest.java
+++ Swp_Klasse4/src/testNG/MyMathTest.java
@@ -16,6 +16,35 @@ public class MyMathTest {
   public void testSubtraction() {
 	  MyMath math = new MyMath();
 	  int result = math.subtract(10, 4);
-	  Assert.assertEquals(result, 5);
+	  Assert.assertEquals(result, 6);
   }
+  
+  @Test
+  public void testMultiplication() {
+	  MyMath math = new MyMath();
+	  int result = math.multiply(3, 5);
+	  Assert.assertEquals(result, 15);
+  }
+  
+  @Test
+  public void testDivision() {
+	  MyMath math = new MyMath();
+	  int result = math.divide(5, 2);
+	  Assert.assertEquals(result, 2);
+  }
+  
+  @Test
+  public void testDoubleDivision() {
+	  MyMath math = new MyMath();
+	  double result = math.divide(1D, 3D);
+	  Assert.assertEquals(result, 1D/3D);
+  }
+  
+  @Test
+  public void testComparison() {
+	  MyMath math = new MyMath();
+	  boolean result = math.comparison(40, 10);
+	  Assert.assertEquals(result, true);
+  }
+  
 }
