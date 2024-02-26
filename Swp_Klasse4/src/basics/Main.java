e4d6e4ff0ddccf974d07055aa62eb6e2dfb71262
juliaPetz <152809377+juliaPetz@users.noreply.github.com>
2024-02-26 20:02:36 +0100

    Create Main.java

---
+++ Swp_Klasse4/src/basics/Main.java
@@ -0,0 +1,31 @@
+package basics;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Test out your Divide() function!
+
+        HashMap<String, String> studentFiles = new HashMap<>();
+        studentFiles.put("Carl", "Program.java");
+        studentFiles.put("Brad", "");
+        studentFiles.put("Elizabeth", "MyCode.java");
+        studentFiles.put("Stefanie", "CoolProgram.java");
+
+        // Test out your CheckFileExtension() function!
+    }
+
+    public static void divide(int x, int y)
+    {
+        // Write code here!
+    }
+
+    public static int checkFileExtension(String fileName)
+    {
+        // Write code here!
+    }
+
+}
