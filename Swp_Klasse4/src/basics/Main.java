4b34fbc27f70a2fa371f8a5aa0e34f8da6476edf
juliaPetz <152809377+juliaPetz@users.noreply.github.com>
2024-02-26 20:03:21 +0100

    Update Main.java
    Updated typos

--- Swp_Klasse4/src/basics/Main.java
+++ Swp_Klasse4/src/basics/Main.java
@@ -18,13 +18,11 @@ public class Main {
         // Test out your CheckFileExtension() function!
     }
 
-    public static void divide(int x, int y)
-    {
+    public static void divide(int x, int y){
         // Write code here!
     }
 
-    public static int checkFileExtension(String fileName)
-    {
+    public static int checkFileExtension(String fileName){
         // Write code here!
     }
 
