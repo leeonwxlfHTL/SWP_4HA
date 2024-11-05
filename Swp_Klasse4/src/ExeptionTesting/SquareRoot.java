9a091cf8cea20f7b4b52adefbe92038940bdc92c
leeonwxlfHTL <leon.wolf@hak-reutte.ac.at>
2024-11-05 10:12:01 +0100

    added SquareRoot classes

---
+++ Swp_Klasse4/src/ExeptionTesting/SquareRoot.java
@@ -0,0 +1,11 @@
+package ExeptionTesting;
+
+public class SquareRoot {
+
+        public double squareRoot(double value) {
+            if(value < 0) {
+                throw new IllegalArgumentException("Number must be greater than or equal to 0");
+            }
+            return Math.sqrt(value);
+        }
+}
---
+++ Swp_Klasse4/src/ExeptionTesting/SquareRootTest.java
@@ -0,0 +1,12 @@
+package ExeptionTesting;
+
+import org.testng.annotations.Test;
+
+public class SquareRootTest {
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testSquareRoot() {
+        SquareRoot squareRoot = new SquareRoot();
+        squareRoot.squareRoot(-1);
+    }
+}
