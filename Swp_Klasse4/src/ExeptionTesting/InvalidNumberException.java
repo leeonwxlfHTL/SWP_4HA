7a68ca992437368b69afa8fd03c16adeebd80ac5
leeonwxlfHTL <leon.wolf@hak-reutte.ac.at>
2024-11-05 10:30:14 +0100

    added InvalidNumberException class

---
+++ Swp_Klasse4/src/ExeptionTesting/InvalidNumberException.java
@@ -0,0 +1,10 @@
+package ExeptionTesting;
+
+public class InvalidNumberException extends Exception{
+
+    private static final long serialVersionUID = 1L;
+
+    public InvalidNumberException(String message) {
+        super(message);
+    }
+}
